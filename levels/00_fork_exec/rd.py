3474e67776a25af7a42214f988f90ee1b0c1eab8
Sukice <3376446591@qq.com>
2026-07-23 14:37:27 +0800

    finish level00

--- levels/00_fork_exec/rd.py
+++ levels/00_fork_exec/rd.py
@@ -26,22 +26,14 @@ def cli():
 
 
 def contain(command):
-    # TODO: exec command, note the difference between the exec flavours
-    #       https://docs.python.org/2/library/os.html#os.execv
-    # NOTE: command is an array (the first element is path/file, and the entire
-    #       array is exec's args)
-
-    os._exit(0)  # TODO: remove this after adding exec
-
+    os.execvp(command[0], command)
 
 @cli.command(context_settings=dict(ignore_unknown_options=True,))
 @click.argument('Command', required=True, nargs=-1)
+
 def run(command):
-    # TODO: replace this with fork()
-    #       (https://docs.python.org/2/library/os.html#os.fork)
-    pid = 0
+    pid = os.fork()
     if pid == 0:
-        # This is the child, we'll try to do some containment here
         try:
             contain(command)
         except Exception:
